7e4cbc1db2e45dfcc4c6d4f880884e1e2b38a6e4
RafeedMohammad <rafeedmohammad1107@gmail.com>
2022-11-18 19:33:47 +0600

    First Commit

--- asset_management/hooks.py
+++ asset_management/hooks.py
@@ -87,9 +87,9 @@ app_license = "MIT"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Asset": "custom_erpnext.asset.override_Asset"
+}
 
 # Document Events
 # ---------------
